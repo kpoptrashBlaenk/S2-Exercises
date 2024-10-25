65dc0eb06448fed10834303d9cbca8fe83fc95d2
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-25 16:05:43 +0200

    Logic: Ex3

--- Exercise 3 JS/assets/js/logic.js
+++ Exercise 3 JS/assets/js/logic.js
@@ -7,3 +7,9 @@
 // var birthYear = +prompt('Your birth year:');
 // var age = new Date().getFullYear() - birthYear;
 // alert(`This will cost you ${age < 12 ? 'nothing!' : age <= 18 || age > 70 ? '70€' : "you don't even wanna know..."}`);
+
+// Exercise 3
+// var countdown = +prompt('Seconds:');
+// for (i = countdown; i >= 0; i--) {
+//   setTimeout(alert(i), 1000)
+// }
