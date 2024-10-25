6d06bb712bd393d25ea1c0b18a41e70a89547a6e
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-25 15:59:08 +0200

    Logic: Ex2

--- Exercise 3 JS/assets/js/logic.js
+++ Exercise 3 JS/assets/js/logic.js
@@ -2,3 +2,8 @@
 // var birthYear = +prompt('Your birth year:')
 // var major = 18 <= new Date().getFullYear() - birthYear
 // alert(major ? 'Get free Hentai here!' : 'Get outta here!')
+
+// Exercise 2
+// var birthYear = +prompt('Your birth year:');
+// var age = new Date().getFullYear() - birthYear;
+// alert(`This will cost you ${age < 12 ? 'nothing!' : age <= 18 || age > 70 ? '70€' : "you don't even wanna know..."}`);
