5223047a44fa02ed2963435bc500991b160047cb
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-31 11:31:43 +0100

    DOM: Ex3

--- Exercise 3 JS/assets/js/dom.js
+++ Exercise 3 JS/assets/js/dom.js
@@ -1,7 +1,7 @@
 // Exercise 1
 var footerDiv = document.querySelector('#footer');
 var footer = document.createElement('footer');
-footerDiv.appendChild(footer);
+footerDiv.append(footer);
 footer.innerHTML = 'Copyright TM';
 footer.style.textAlign = 'center';
 
@@ -12,3 +12,22 @@ footer.style.textAlign = 'center';
 // blocEx2.style.color = hexadecimal;
 // blocEx2.style.backgroundColor = hexadecimal;
 // blocEx2.innerText = text;
+
+// Exercise 3
+// var blocEx3 = document.querySelector('#blocEx3');
+// var divEx3 = document.createElement('div');
+// blocEx3.append(divEx3);
+
+// var ulEx3 = document.createElement('ul');
+// divEx3.append(ulEx3);
+// while (website !== 'stop') {
+//   var website = prompt('Give me a website:');
+//   if (website !== 'stop') {
+//     var li = document.createElement('li');
+//     var a = document.createElement('a');
+//     a.href = website;
+//     a.innerText = website;
+//     li.append(a);
+//     ulEx3.append(li);
+//   }
+// }
