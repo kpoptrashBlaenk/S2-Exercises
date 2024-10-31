137858e2f70892836223d4cd3c46f11bebcee15c
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-31 11:17:33 +0100

    DOM: Ex2

--- Exercise 3 JS/assets/js/dom.js
+++ Exercise 3 JS/assets/js/dom.js
@@ -1,7 +1,14 @@
 // Exercise 1
-
 var footerDiv = document.querySelector('#footer');
 var footer = document.createElement('footer');
 footerDiv.appendChild(footer);
 footer.innerHTML = 'Copyright TM';
 footer.style.textAlign = 'center';
+
+// Exercise 2
+// var hexadecimal = prompt('Give a color in hexadecimal:');
+// var text = prompt('Give me some text:');
+// var blocEx2 = document.querySelector('#blocEx2');
+// blocEx2.style.color = hexadecimal;
+// blocEx2.style.backgroundColor = hexadecimal;
+// blocEx2.innerText = text;
