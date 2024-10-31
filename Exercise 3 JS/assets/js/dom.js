4ae514d8ca6d5b69fb24ee3f54ec5022e125d644
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-31 11:10:27 +0100

    DOM: Ex1

---
+++ Exercise 3 JS/assets/js/dom.js
@@ -0,0 +1,7 @@
+// Exercise 1
+
+var footerDiv = document.querySelector('#footer');
+var footer = document.createElement('footer');
+footerDiv.appendChild(footer);
+footer.innerHTML = 'Copyright TM';
+footer.style.textAlign = 'center';
