9a00b2815d047c1e406b23e43650a32c6b16c209
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-31 12:05:32 +0100

    DOM: uncomment

--- Exercise 3 JS/assets/js/dom.js
+++ Exercise 3 JS/assets/js/dom.js
@@ -6,60 +6,60 @@ footer.innerHTML = 'Copyright TM';
 footer.style.textAlign = 'center';
 
 // Exercise 2
-// var hexadecimal = prompt('Give a color in hexadecimal:');
-// var text = prompt('Give me some text:');
-// var blocEx2 = document.querySelector('#blocEx2');
-// blocEx2.style.color = hexadecimal;
-// blocEx2.style.backgroundColor = hexadecimal;
-// blocEx2.innerText = text;
+var hexadecimal = prompt('Give a color in hexadecimal:');
+var text = prompt('Give me some text:');
+var blocEx2 = document.querySelector('#blocEx2');
+blocEx2.style.color = hexadecimal;
+blocEx2.style.backgroundColor = hexadecimal;
+blocEx2.innerText = text;
 
 // Exercise 3
-// var blocEx3 = document.querySelector('#blocEx3');
-// var divEx3 = document.createElement('div');
-// blocEx3.append(divEx3);
+var blocEx3 = document.querySelector('#blocEx3');
+var divEx3 = document.createElement('div');
+blocEx3.append(divEx3);
 
-// var ulEx3 = document.createElement('ul');
-// divEx3.append(ulEx3);
-// while (website !== 'stop') {
-//   var website = prompt('Give me a website:');
-//   if (website !== 'stop') {
-//     var li = document.createElement('li');
-//     var a = document.createElement('a');
-//     a.href = website;
-//     a.innerText = website;
-//     li.append(a);
-//     ulEx3.append(li);
-//   }
-// }
+var ulEx3 = document.createElement('ul');
+divEx3.append(ulEx3);
+while (website !== 'stop') {
+  var website = prompt('Give me a website:');
+  if (website !== 'stop') {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.href = website;
+    a.innerText = website;
+    li.append(a);
+    ulEx3.append(li);
+  }
+}
 
 // Exercise 4
-// function createOnesAndAppend(color) {
-//   var span = document.createElement('span');
-//   var br = document.createElement('br');
-//   var text = '';
+function createOnesAndAppend(color) {
+  var span = document.createElement('span');
+  var br = document.createElement('br');
+  var text = '';
 
-//   for (var j = 0; j < i; j++) {
-//     text += '1';
-//   }
+  for (var j = 0; j < i; j++) {
+    text += '1';
+  }
 
-//   span.innerText = text;
-//   span.style.color = color;
-//   blocEx4.append(span);
-//   blocEx4.append(br);
-// }
+  span.innerText = text;
+  span.style.color = color;
+  blocEx4.append(span);
+  blocEx4.append(br);
+}
 
-// var number = +prompt('Give a number');
-// var color = prompt('Give a color');
-// var color2 = prompt('Give a second color');
-// var blocEx4 = document.querySelector('#blocEx4');
+var number = +prompt('Give a number');
+var color = prompt('Give a color');
+var color2 = prompt('Give a second color');
+var blocEx4 = document.querySelector('#blocEx4');
 
-// for (var i = 1; i < number + 1; i++) {
-//   createOnesAndAppend(color);
-// }
+for (var i = 1; i < number + 1; i++) {
+  createOnesAndAppend(color);
+}
 
-// for (var i = number; i > 0; i--) {
-//   createOnesAndAppend(color2);
-// }
+for (var i = number; i > 0; i--) {
+  createOnesAndAppend(color2);
+}
 
 // Exercise 5
 var blocEx5 = document.querySelector('#blocEx5');
@@ -85,20 +85,20 @@ for (var i = 0; i < characters.length; i++) {
 
 blocEx5.append(ul);
 
-// while (character !== 'stop') {
-//   var character = prompt('Give me a character name:');
+while (character !== 'stop') {
+  var character = prompt('Give me a character name:');
 
-//   if (character !== 'stop') {
-//     var online = confirm('Is the character online?');
-//     var character = {
-//       name: character,
-//       online: online,
-//     };
+  if (character !== 'stop') {
+    var online = confirm('Is the character online?');
+    var character = {
+      name: character,
+      online: online,
+    };
 
-//     var li = createCharacter(character);
-//     ul.append(li);
-//   }
-// }
+    var li = createCharacter(character);
+    ul.append(li);
+  }
+}
 
 function createCharacter(character) {
   var li = document.createElement('li');
