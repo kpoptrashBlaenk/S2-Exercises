a5ac3bdbc46e7a75bb2a22b6cfc3bd08d3320694
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-31 12:31:59 +0100

    Events: Ex2

--- Exercise 3 JS/assets/js/events.js
+++ Exercise 3 JS/assets/js/events.js
@@ -9,3 +9,26 @@ button.addEventListener('click', () => {
   console.log(clicks);
   iterations.innerText = clicks;
 });
+
+// Exercise 2
+var submitButton = document.querySelector('#sendForm');
+var formFirstName = document.querySelector('#firstName');
+var formLastName = document.querySelector('#lastName');
+var formBirthday = document.querySelector('#birthday');
+var submitResults = document.querySelector('#results');
+var resFirstName = document.querySelector('#resFirstName');
+var resLastName = document.querySelector('#resLastName');
+var resAge = document.querySelector('#resAge');
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  submitResults.classList.remove('invisible');
+  resFirstName.innerText = `First Name: ${formFirstName.value}`;
+  resLastName.innerText = `Last Name: ${formLastName.value}`;
+
+  // I still hate that Date class has no date calculation
+  var ageInMilliseconds = new Date() - new Date(formBirthday.value);
+  var ageInYears = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  resAge.innerText = `Age: ${ageInYears}`;
+});
